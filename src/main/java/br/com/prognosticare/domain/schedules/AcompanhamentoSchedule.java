ae6ff25f00376c0ce0277405478fe4598cb9fea0
AlissonFerreiraEvangelista <alisson.22559@gmail.com>
2023-10-31 19:10:18 -0300

    Ajuste no AgendaSchedule

--- src/main/java/br/com/prognosticare/domain/schedules/AcompanhamentoSchedule.java
+++ src/main/java/br/com/prognosticare/domain/schedules/AcompanhamentoSchedule.java
@@ -55,6 +55,7 @@ public class AcompanhamentoSchedule {
                             .build())
                     .setToken(tokenFCM)
                     .build();
+                    
     
             try {
                 firebaseMessaging.send(message);
